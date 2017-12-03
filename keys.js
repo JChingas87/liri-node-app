24c5c38b184b9fdebea2f66d7df85e0f47f82c03
Joseph A Chingas <JChingas87@Josephs-MacBook-Pro.local>
2017-12-02 19:33:41 -0500

    packages and API's

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: '	QlfN06XgzUYlKDieIP57c00eL',
+    consumer_secret: 'EPfVyPKkb2cr945EbVownR2t3mYCIXz0BumwW7fR9IZkN6sp8S',
+    access_token_key: 'shEva30l1h0eCYYqoZqP0dkqtogjsojcOMPnewN',
+    access_token_secret: 'yIkz0hMoDFiLmi4xWT61JjQS9gtNu51lhnbsknPZhlmiF',
+}
+
+module.exports = twitterKeys;
---
+++ liri-app.js
@@ -0,0 +1,5 @@
+var keys = require('./keys.js');
+var inquirer = require('inquirer');
+var spotify = require('node-spotify-api');
+var request = require('request');
+var twitter = require('twitter');
